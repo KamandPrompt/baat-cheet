ac8319cfe55768865f5877b76d87d9cd88e01237
Hitesh Ramchandani <hitr9831@gmail.com>
2017-01-27 01:14:50 +0530

    Initialize server side implementation with app.js
    * Initialize server side implementation
    
    * Add more functionality

---
+++ app.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+var users = [];
+
+io.on('connection', function(socket) {
+
+	socket.on('set username', function(name) {
+		//username not taken
+		if(users.indexOf(name) == -1) {
+			users.push(name.trim());
+			socket.emit('user set', name);
+			socket.username = name;
+		}	
+		//username taken
+		else {
+			socket.emit('user exists', name);
+		}
+	});
+
+	//When user disconnets remove user from users
+	socket.on('disconnect', function() {
+		var index = users.indexOf(socket.username);
+		if(index != -1) {
+			users.splice(index, 1);
+		}
+	});
+});
+
+http.listen('3000', function() {
+	console.log('listening on *3000');
+});
