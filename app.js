16a26c4cf1175512e135b1ed761b074233ef86be
Hitesh Ramchandani <hitr9831@gmail.com>
2017-01-27 00:12:55 +0530

    Add more functionality

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var path = require('path');
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'temp.html'));
 });
 
 var users = [];
@@ -14,7 +14,9 @@ io.on('connection', function(socket) {
 	socket.on('set username', function(name) {
 		//username not taken
 		if(users.indexOf(name) == -1) {
+			users.push(name.trim());
 			socket.emit('user set', name);
+			socket.username = name;
 		}	
 		//username taken
 		else {
@@ -22,6 +24,13 @@ io.on('connection', function(socket) {
 		}
 	});
 
+	//When user disconnets remove user from users
+	socket.on('disconnect', function() {
+		var index = users.indexOf(socket.username);
+		if(index != -1) {
+			users.splice(index, 1);
+		}
+	});
 });
 
 http.listen('3000', function() {
