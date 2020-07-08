4ad4871ece4e93be9f174a95d1fef6b73fc5b5b1
Lalit Narayan Mudgal <lalitrpvv@gmail.com>
2020-07-08 23:32:26 +0530

    Message Input bar not visible in Rooms
    images removed font-awesome icons added
    
    Improved sidebar UX
    
    Increased font size in add lobby page
    
    Fix merge
    
    Signed-off-by: Lais Frigério <laiss@gazetadopovo.com.br>
    
    Message Input bar not visible in Rooms

--- public/client.js
+++ public/client.js
@@ -7,36 +7,45 @@ function setUsername() {
 };
 
 //sends a message
-function sendMessage() {
-    msg = $('#textarea').val(); 
-    //alert(msg);
+function sendMessage(msg) {
+    // alert(msg);
     msg = msg.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     room_name = $(".active").attr("id");
-    socket.emit('Message Request', {msg: msg, room: room_name});
+    socket.emit('Message Request', {
+        msg: msg,
+        room: room_name
+    });
 }
 
 //creates a room
 function createRoom() {
-    if($("#roomName").val()=='') return ; 
-    socket.emit('create room', {room_name: $('#roomName').val(), description: $('#description').val()});
+    if ($("#roomName").val() == '') return;
+    socket.emit('create room', {
+        room_name: $('#roomName').val(),
+        description: $('#description').val()
+    });
 }
 
 //requests server to join a room
-function joinRoom(room){
+function joinRoom(room) {
     var room_id = convertIntoId(room);
-    socket.emit('join room', {name:room} );
+    socket.emit('join room', {
+        name: room
+    });
     $(".error").hide();
-    $("#" + room_id + "-msg").attr("data-joined",1);
+    $("#" + room_id + "-msg").attr("data-joined", 1);
     $("#" + room_id + "-msg,.write").show();
 }
 
 //requests server to leave a room
-function leaveRoom(room){
+function leaveRoom(room) {
     var room_id = convertIntoId(room);
-    socket.emit('leave room', {name:room} );
+    socket.emit('leave room', {
+        name: room
+    });
     $(".error").html('<span id="error">You havent joined this room yet. <button onclick="joinRoom( \'' + room + '\' )" id="joinBtn">Join<Button/> to see the conversation.</span>');
-    
-    $("#" + room_id + "-msg").attr("data-joined",0);
+
+    $("#" + room_id + "-msg").attr("data-joined", 0);
     $("#" + room_id + "-msg,.write").hide();
     $(".error").show();
 }
@@ -46,23 +55,30 @@ function convertIntoId(name) {
     return name.replace(/[!"#$%&'()*+,.\/:;<=>?@[\\\]^{|}~ ]/g, "\\$&");
 }
 
+
+//convert an array into list elements
+function convertIntoList(arr) {
+    var list = ('<ul>');
+    for(var i=0; i<arr.length; i++)	list = list.concat('<li>' + arr[i] + '</li>');
+    list = list.concat('</ul>');
+    return list;
+}
+
 //if server emits user exists, propmt for changing username
-socket.on('user exists', function (data) {
+socket.on('user exists', function(data) {
     document.getElementById('error_response').innerHTML = data + ' username already taken! Try another one.'
 });
 
 //if server emits user set, display rooms to user
-socket.on('user set', function (data) {
+socket.on('user set', function(data) {
     username = data.username;
     var date = new Date()
     $("#user").fadeOut();
-    $("body").css("background-color","#f8f8f8");
+    $("body").css("background-color", "#f8f8f8");
     $(".wrapper").fadeIn();
-    $(".chat[data-chat='person1']").append("<div class='conversation-start'>\
-                                                <span>" + date.getHours() + ':' + date.getMinutes() + "</span>\
-                                            </div>");
     // $(".top[data-chat='person1']").("<center><span> " + data.online + " user(s) online</span><center>");
     $(".top[data-chat='person1']").find("span")[1].innerHTML = data.online + " user(s) online</span>";
+    $(".Participants").find('span')[0].innerHTML = convertIntoList(data.online_users);
     socket.username = data.username;
     scrollDiff = $("#lobby-msg").children(".chat")[0].scrollHeight;
 });
@@ -70,7 +86,8 @@ socket.on('user set', function (data) {
 //notifies users that someone joined baat-cheet
 socket.on('user joined', function(data) {
     $.notify(data.username + " just joined", "info");
-        $("#lobby-msg").find('.top').find('span')[1].innerHTML = data.online + " user(s) online";
+    $("#lobby-msg").find('.top').find('span')[1].innerHTML = data.online + " user(s) online";
+    $(".Participants").find('span')[0].innerHTML = convertIntoList(data.online_users);
 });
 
 //notifies users that someone left
@@ -82,9 +99,10 @@ socket.on('user left', function(data) {
 //notifies users that someone joined a room
 socket.on('user join', function(data) {
     var room_id = convertIntoId(data.room);
-    if(data.room!="lobby"){
+    if (data.room != "lobby") {
         $.notify(data.username + " just joined " + data.room + " room!", "info");
         $("#" + room_id + "-msg").find('.top').find('span')[1].innerHTML = data.online + " user(s) online";
+        $("#" + room_id + "-msg").find('.Participants').find('span')[0].innerHTML = convertIntoList(data.online_users);
     }
 });
 
@@ -92,42 +110,57 @@ socket.on('user join', function(data) {
 socket.on('Display Message', function(data) {
     var today = new Date();
     var class_name;
-    if(socket.username == data.user) {
+    if (socket.username == data.user) {
         class_name = 'self';
-    }
-    else {
+    } else {
         class_name = 'others'
     }
     // Adding Emoji
     var p = data.msg;
     var colon1 = p.indexOf(":");
     while (colon1 != -1) {
-      var colon2 = p.indexOf(":", colon1 + 1);
-      if (colon2 != -1) {
-        emoji = p.slice(colon1+1, colon2);
-        var p = p.slice(0, colon1) + "<img class=\"emoji\" src=\"images/emoji/" + emoji + ".png\">" + p.slice(colon2 + 1);
-        colon1 = p.indexOf(":", colon2 + 1);
-      }
-      else {
-        break;
-      }
+        var colon2 = p.indexOf(":", colon1 + 1);
+        if (colon2 != -1) {
+            emoji_name = p.slice(colon1 + 1, colon2);
+            position = emoji_names.indexOf(emoji_name)
+            if (position != -1) {
+                p = p.slice(0, colon1) + "<img class=\"emoji\" src=\"images/emoji/" + emojis[position] + ".png\">" + p.slice(colon2 + 1);
+            }
+            colon1 = p.indexOf(":", colon2 + 1);
+        } else {
+            break;
+        }
     }
-    p = "<p>" + p + "</p>";
+    var dateTime = new Date();
+    var hours = dateTime.getHours().toString(10);
+    var mins = dateTime.getMinutes().toString(10);
+    if (hours.length == 1) {
+        hours = '0' + hours;
+    }
+    if (mins.length == 1) {
+        mins = '0' + mins;
+    }
+    // Format Message
+    const div = document.createElement('div');
+    const username = document.createElement('small');
+    const timestamp = document.createElement('small');
+    const br = document.createElement('br');
+    div.classList.add("bubble", class_name);
+    username.classList.add("info");
+    timestamp.classList.add("info");
+    div.setAttribute("data-chat", "person1")
+    username.innerText = data.user;
+    timestamp.innerText = hours + ":" + mins;
+    if (class_name == 'self') {
+        div.innerHTML += p + br.outerHTML + timestamp.outerHTML;
+    } else {
+        div.innerHTML += username.outerHTML + br.outerHTML + p + br.outerHTML + timestamp.outerHTML;
+    }
     var room_id = convertIntoId(data.room);
-    if(class_name == 'self' || ($("#" + room_id + "-msg").children(".chat")[0].scrollHeight - $("#" + room_id + "-msg").children(".chat").scrollTop() == scrollDiff)){
-        $("#"+ room_id +"-msg").children(".chat[data-chat='person1']").append("<div class='bubble " + class_name + "' data-chat ='person1'>\
-                                " + p + "<br>\
-                                <span class='info'>" + data.user + "</span>\
-                           </div>");
-        var room_id = convertIntoId($(".active").attr("id"));
-        var height = $("#" + room_id + "-msg").children(".chat")[0].scrollHeight;
-        $("#" + room_id + "-msg").children(".chat").scrollTop(height);
-    }
-    else{$("#"+ room_id +"-msg").children(".chat[data-chat='person1']").append("<div class='bubble " + class_name + "' data-chat ='person1'>\
-                                " + p + "<br>\
-                                <span class='info'>" + data.user + "</span>\
-                           </div>");
-    }
+    $("#" + room_id + "-msg").children(".chat[data-chat='person1']").append(div)
+    var room_id = convertIntoId($(".active").attr("id"));
+    var height = $("#" + room_id + "-msg").children(".chat")[0].scrollHeight;
+    $("#" + room_id + "-msg").children(".chat").scrollTop(height);
 });
 
 //if room exists, then prompt for another room name
@@ -137,20 +170,42 @@ socket.on('room exists', function(data) {
 
 //displays room to the creator
 socket.on('room created self', function(data) {
+    const { description, room_name, online, online_users } = data;
     var date = new Date();
-    var room_id = convertIntoId(data.room_name);
-    $('.people').append("<li class='person' data-chat='person1' id='" + data.room_name + "' onclick='showRoom(this)'>\
-                            <span class='name'>" + data.room_name + "</span><br>\
-                            <span class='preview'>" + data.description + "</span>\
-                        </li>");
-    $('.container').append("<div class='right' id='" + data.room_name + "-msg" + "' data-joined='1' style='display:none;'>\
-            <div class='top'><center id='online'><span>" + data.room_name + " Room</span>&nbsp;(<a href='#' onclick='leaveRoom(\"" + data.room_name + "\")'>Leave room</a>)</center></div>\
-                            <div class='chat active-chat' data-chat='person1'></div>\
-        </div>");
-    $("#"+ room_id +"-msg").children(".chat[data-chat='person1']").append("<div class='conversation-start'>\
+    var room_id = convertIntoId(room_name);
+    
+    const $userInfo = `
+                        <li class='person' data-chat='person1' id='${room_name}' onclick='showRoom(this)'>
+                            <span class='name'>${room_name}</span><br>
+                            <span class='preview'>${description}</span>
+                        </li>
+                    `;
+    $('.people').append($userInfo);
+
+    const $write = $("#write");
+
+    const $contentInfo = `
+                            <div class='right' id='${room_name}-msg' data-joined='1' style='display:none;'>
+                                <div class='top'><center id='online'><span>${room_name} Room</span>&nbsp;(<a href='#' onclick='leaveRoom("${room_name}")'>Leave room</a>)</center>
+                                    <center><button class='btn' onclick='collap(${room_name})'><span>${online} user online</span></button></center>
+                                </div>
+                                <div class='Participants'>
+                                    <center><h2>Participants</h2></center>
+                                    <span></span>
+                                </div>
+                                <div class='chat active-chat' data-chat='person1'></div>
+                                <div class="write">
+                                    ${$write.html()}
+                                </div>
+                            </div>
+                        `;
+
+    $('.app-container').append($contentInfo);
+    
+    $(`#${room_id}-msg`).children(".chat[data-chat='person1']").append("<div class='conversation-start'>\
                                                 <span>" + date.getHours() + ':' + date.getMinutes() + "</span>\
                                             </div>");
-    $("#" + room_id + "-msg").find('.top').append("<center><span> " + data.online + " users online</span></center>");
+    $(`#${room_id}-msg`).find('.Participants').find('span')[0].innerHTML = convertIntoList(online_users);
     $("#room").fadeOut();
     $(".wrapper").fadeIn();
     $('#roomName').val("");
@@ -159,21 +214,41 @@ socket.on('room created self', function(data) {
 
 //displays room to the others
 socket.on('room created other', function(data) {
-    if(username) {
+    if (username) {
+        const { description, room_name, online, online_users } = data;
         var date = new Date();
-        var room_id = convertIntoId(data.room_name);
-        $('.people').append("<li class='person' data-chat='person1' id='" + data.room_name + "' onclick='showRoom(this)'>\
-                                <span class='name'>" + data.room_name + "</span><br>\
-                                <span class='preview'>" + data.description + "</span>\
-                            </li>");
-        $('.container').append("<div class='right' id='" + data.room_name + "-msg" + "'  data-joined='0' style='display:none;'>\
-                <div class='top'><center id='online'><span>" + data.room_name + " Room</span>&nbsp;(<a href='#' onclick='leaveRoom(\"" + data.room_name + "\")'>Leave room</a>)</center></div>\
-                                <div class='chat active-chat' data-chat='person1'></div>\
-            </div>");
-         $("#"+ room_id +"-msg").children(".chat[data-chat='person1']").append("<div class='conversation-start'>\
+        var room_id = convertIntoId(room_name);
+        
+        const $write = $("#write");
+        const $userInfo = `
+                            <li class='person' data-chat='person1' id='${room_name}' onclick='showRoom(this)'>
+                                <span class='name'>${room_name}</span><br>
+                                <span class='preview'>${description}</span>
+                            </li>
+                        `;
+        $('.people').append($userInfo);
+
+        const $contentInfo = `
+                            <div class='right' id='${room_name}-msg' data-joined='0' style='display:none;'>
+                                <div class='top'><center id='online'><span>${room_name} Room</span>&nbsp;(<a href='#' onclick='leaveRoom("${room_name}")'>Leave room</a>)</center>
+                                    <center><button class='btn' onclick='collap(${room_name})'><span>${online} users online</span></button></center>
+                                </div>
+                                <div class='Participants'>
+                                    <center><h2>Participants</h2></center>
+                                    <span></span>
+                                </div>
+                                <div class='chat active-chat' data-chat='person1'></div>
+                                <div class="write">
+                                    ${$write.html()}
+                                </div>
+                            </div>
+                        `;
+
+        $('.app-container').append($contentInfo);
+        $(`#${room_id}-msg`).children(".chat[data-chat='person1']").append("<div class='conversation-start'>\
                                                     <span>" + date.getHours() + ':' + date.getMinutes() + "</span>\
                                                 </div>");
-          $("#" + room_id + "-msg").find('.top').append("<center><span> " + data.online + " users online</span></center>");
+        $(`#${room_id}-msg`).find('.Participants').find('span')[0].innerHTML = convertIntoList(online_users);
     }
 });
 
@@ -181,13 +256,13 @@ socket.on('room created other', function(data) {
 socket.on('destroy room', function(data) {
 
     //redirect user to lobby if the active room is to be destroyed
-    if($(".active").attr("id") == data) {
+    if ($(".active").attr("id") == data) {
         $("#lobby").addClass('active');
         $("#lobby-msg").css("display", "inherit");
     }
 
     $(".error").hide();
-    $(".write").css("display","initial");
+    $(".write").css("display", "initial");
 
     var room_id = convertIntoId(data);
     $('#' + room_id).remove();
@@ -199,20 +274,24 @@ socket.on('user left room', function(data) {
     var room_id = convertIntoId(data.room);
     $.notify(data.username + " just left room " + data.room, "error");
     $("#" + room_id + "-msg").find('.top').find('span')[1].innerHTML = data.online + " user(s) online";
+    $("#" + room_id + "-msg").find('.Participants').find('span')[0].innerHTML = convertIntoList(data.online_users);
 });
 
 //updates info about number of users
 socket.on('update info', function(rooms) {
     var room_id;
     // alert(rooms);
-    for(var i=0; i<rooms.length; i++) {
+    for (var i = 0; i < rooms.length; i++) {
         room_id = convertIntoId(rooms[i].name);
         $("#" + room_id + "-msg").find('.top').find('span')[1].innerHTML = rooms[i].num_users + " user(s) online";
+        $("#" + room_id + "-msg").find('.Participants').find('span')[0].innerHTML = convertIntoList(rooms[i].users);
     }
 });
 
 //updates info about number of users
 socket.on('room joined', function(data) {
-    var room_id = convertIntoId(data.name);
-    $("#" + room_id + "-msg").find('.top').find('span')[1].innerHTML = data.online + " user(s) online";
-}); 
+    const { name, online, online_users } = data;
+    var room_id = convertIntoId(name);
+    $("#" + room_id + "-msg").find('.top').find('span')[1].innerHTML = online + " user(s) online";
+    $("#" + room_id + "-msg").find('.Participants').find('span')[0].innerHTML = convertIntoList(online_users);
+});
--- public/script.js
+++ public/script.js
@@ -1,96 +1,151 @@
-$(document).ready(function(){
-$("#userN").on("keypress", function(val) {
-           if(val.which == 13) {
-               setUsername();
-           }
-       });
+$(document).ready(function() {
+  $("#userN").on("keypress", function(val) {
+    if (val.which == 13) {
+      setUsername();
+    }
+  });
 
-       $('#textarea').on("keypress", function(val) {
-           if(val.which == 13) {
-               sendMessage();
-               $(this).val("");
-           }
-       });
+  // event listener to dinamic element
+  $("body").on("keypress", '.textarea', function(val) {
+    if (val.which == 13) {
+      sendMessage($(this).val());
+      $(this).val("");
+    }
+  });
 
-       $('.send').click(function(){
-          sendMessage();
-          $('#textarea').val("");
-       });
-    });
+  $("body").on("click", '.send', function() {
+    var $input = $(this).parents('.write').find('.textarea');
+    sendMessage($input.val());
+    $input.val("");
+  });
+});
 
 $(".searchtext").on("keyup", function(val) {
   search();
 });
 
-$(".search").click(function(){
-    search();
+$(".search").click(function() {
+  search();
 });
 
-function search()
-{
-    var $searchtext =$(".searchtext").val().toLowerCase();
-    $("ul[name='people'] li").each(function(){
-        var st = $(this).children(".name").text().toLowerCase();
-        var $pt=$.trim(st);
-       if($pt.includes($searchtext))
-          $(this).show();
-        else
-          $(this).hide();
-    });
+function search() {
+  var $searchtext = $(".searchtext").val().toLowerCase();
+  $("ul[name='people'] li").each(function() {
+    var st = $(this).children(".name").text().toLowerCase();
+    var $pt = $.trim(st);
+    if ($pt.includes($searchtext))
+      $(this).show();
+    else
+      $(this).hide();
+  });
 };
 
-function newRoom(){
-    $("#room").css("display","block");
+function newRoom() {
+  $("#room").css("display", "block");
 };
 
 $("#cancel").click(function() {
-    $(this).parent().parent().hide();
+  $(this).parent().parent().hide();
 });
 
-function submitRoom(){
-    createRoom(); // used in client.js to pass details to app.js
+function submitRoom() {
+  createRoom(); // used in client.js to pass details to app.js
 };
 
-function showRoom(name){
-    var current_room_id = convertIntoId($(".active").attr("id"));
-    $("#"+ current_room_id +"-msg").css("display", "none"); 
-    $(".active").removeClass('active');
-    var room = name.id;
-    var room_id = convertIntoId(room);
-    $("#" + room_id).addClass('active');
-    $("#" + room_id + "-msg").css("display", "inherit");
-    if($("#" + room_id + "-msg").attr("data-joined") == 0){
-        $(".error").css("display","inherit");
-        $(".error").html('<span id="error">You havent joined this room yet. <input type="button" onclick="joinRoom(\'' + name.id + '\')" value="Join" id="joinBtn"/> to see the conversation.</span>');
-        $("#" + room_id + "-msg,.write").hide();
-    }else {
-        $(".error").hide();
-        $(".write").css("display","initial");
-    }
+function showRoom(name) {
+  var current_room_id = convertIntoId($(".active").attr("id"));
+  $("#" + current_room_id + "-msg").css("display", "none");
+  $(".active").removeClass('active');
+  var room = name.id;
+  var room_id = convertIntoId(room);
+  $("#" + room_id).addClass('active');
+  $("#" + room_id + "-msg").css("display", "inherit");
+  if ($("#" + room_id + "-msg").attr("data-joined") == 0) {
+    $(".error").css("display", "inherit");
+    $(".error").html('<span id="error">You havent joined this room yet. <input type="button" onclick="joinRoom(\'' + name.id + '\')" value="Join" id="joinBtn"/> to see the conversation.</span>');
+    $("#" + room_id + "-msg,.write").hide();
+  } else {
+    $(".error").hide();
+    $(".write").css("display", "initial");
+  }
 
 };
 
 $('.chat[data-chat=person1]').addClass('active-chat');
 $('.person[data-chat=person1]').addClass('active');
 
+function collap(room_id) {
+  room_id = convertIntoId(room_id);
+  var height = $("#" + room_id + "-msg").find('.Participants').css('height');
+  if(height == '0px') {
+   $("#" + room_id + "-msg").find('.Participants').css({"height":"60%" , "z-index":"+10"});
+  $("#" + room_id + "-msg").find('.btn').addClass('viewUsers');
+  } else {
+   $("#" + room_id + "-msg").find('.Participants').css({"height":"0%" , "z-index":"-10"});
+  $("#" + room_id + "-msg").find('.btn').removeClass('viewUsers');
+  }
+}
 
 var emo = document.querySelector(".smiley");
 
-function active () {
-  if($(".smiley").hasClass("active")){
+function active() {
+  if ($(".smiley").hasClass("active")) {
     $(".smiley").removeClass("active");
-    $("#emobox").css("display","none");
-    }
-  else{
+    $("#emobox").css("display", "none");
+  } else {
     $(".smiley").addClass("active");
-    $("#emobox").css("display","block");
+    $("#emobox").css("display", "block");
   }
 }
 
 emo.addEventListener("click", active, false);
 
-function writeEmoji(emoji){
+function writeEmoji(emoji) {
   var emojiName = emoji.id;
   $("#textarea").val($("#textarea").val() + ":" + emojiName + ":");
   $("#textarea").focus();
+}
+
+// For sidebar to work on small screens
+
+function closeSidebar() {
+  const navbar = document.querySelector(".left");
+  navbar.style.left = '-300px';
+  right.style.width = "100%";
+  var toggle = document.querySelector("#toggle-icon");
+  toggle.className = '';
+  toggle.className += 'fa fa-angle-right';
+}
+
+function openSidebar() { 
+  const navbar = document.querySelector(".left");
+  navbar.style.left = '0px';
+  right.style.width = "calc(100% - 300px)";
+  var toggle = document.querySelector("#toggle-icon");
+  toggle.className = '';
+  toggle.className += 'fa fa-angle-left';
+}
+
+function SidebarToggle() {
+  const navbar = document.querySelector(".left");
+  if (navbar.style.left == '0px') {
+    closeSidebar();
+  } 
+  else {
+    openSidebar();
+    document.body.addEventListener('click', function(event) {
+      var sidebar = document.getElementsByClassName('left')[0]; //Ensuring that clicks inside the sidebar(outside lobbies) don't make the sidebar collapse
+      var lobbyName = document.getElementsByClassName('person'); //In case user clicks a lobby from the sidebar, the sidebar collapse. This is a list of classes
+      var addLobbyPage = document.getElementById('room'); //Ensuring that clicks on page to add lobby names doesn't make the sidebar collapse
+      for (var i=0; i<lobbyName.length; i++) { 
+        if(lobbyName[i].contains(event.target)) {
+          closeSidebar(); 
+          return;
+        }
+      }
+      if(!sidebar.contains(event.target) && !addLobbyPage.contains(event.target)) { 
+        closeSidebar(); 
+      }
+    }, false); 
+  }
 }
