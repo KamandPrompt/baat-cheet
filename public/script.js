4149bd6b3607d910e14a8a9d9a76d216ecbc2e4b
Ishan Manandhar <ishan02016@gmail.com>
2020-10-07 20:06:42 +0545

    Make new room screen-overlay responsive alongwith responsive simpler sidebar slide

--- public/script.js
+++ public/script.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
-  $('#userN').on('keypress', function (val) {
+  $("#userN").on("keypress", function (val) {
     if (val.which == 13) {
       setUsername();
     }
   });
 
-  $('#textarea').on('keypress', function (val) {
+  $("#textarea").on("keypress", function (val) {
     if (val.which == 13) {
       sendMessage();
-      $(this).val('');
+      $(this).val("");
     }
   });
 
-  $('.send').click(function () {
+  $(".send").click(function () {
     sendMessage();
-    $('#textarea').val('');
+    $("#textarea").val("");
   });
 });
 
-$('.searchtext').on('keyup', function (val) {
+$(".searchtext").on("keyup", function (val) {
   search();
 });
 
-$('.search').click(function () {
+$(".search").click(function () {
   search();
 });
 
 function search() {
-  var $searchtext = $('.searchtext').val().toLowerCase();
+  var $searchtext = $(".searchtext").val().toLowerCase();
   $("ul[name='people'] li").each(function () {
-    var st = $(this).children('.name').text().toLowerCase();
+    var st = $(this).children(".name").text().toLowerCase();
     var $pt = $.trim(st);
     if ($pt.includes($searchtext)) $(this).show();
     else $(this).hide();
@@ -37,10 +37,10 @@ function search() {
 }
 
 function newRoom() {
-  $('#room').css('display', 'block');
+  $("#room").css("display", "block");
 }
 
-$('#cancel').click(function () {
+$("#cancel").click(function () {
   $(this).parent().parent().hide();
 });
 
@@ -49,92 +49,80 @@ function submitRoom() {
 }
 
 function showRoom(name) {
-  var current_room_id = convertIntoId($('.active').attr('id'));
-  $('#' + current_room_id + '-msg').css('display', 'none');
-  $('.active').removeClass('active');
+  var current_room_id = convertIntoId($(".active").attr("id"));
+  $("#" + current_room_id + "-msg").css("display", "none");
+  $(".active").removeClass("active");
   var room = name.id;
   var room_id = convertIntoId(room);
-  $('#' + room_id).addClass('active');
-  $('#' + room_id + '-msg').css('display', 'inherit');
-  if ($('#' + room_id + '-msg').attr('data-joined') == 0) {
-    $('.error').css('display', 'inherit');
-    $('.error').html(
+  $("#" + room_id).addClass("active");
+  $("#" + room_id + "-msg").css("display", "inherit");
+  if ($("#" + room_id + "-msg").attr("data-joined") == 0) {
+    $(".error").css("display", "inherit");
+    $(".error").html(
       '<span id="error">You havent joined this room yet. <input type="button" onclick="joinRoom(\'' +
         name.id +
         '\')" value="Join" id="joinBtn"/> to see the conversation.</span>'
     );
-    $('#' + room_id + '-msg,.write').hide();
+    $("#" + room_id + "-msg,.write").hide();
   } else {
-    $('.error').hide();
-    $('.write').css('display', 'initial');
+    $(".error").hide();
+    $(".write").css("display", "initial");
   }
 }
 
-$('.chat[data-chat=person1]').addClass('active-chat');
-$('.person[data-chat=person1]').addClass('active');
+$(".chat[data-chat=person1]").addClass("active-chat");
+$(".person[data-chat=person1]").addClass("active");
 
 function collap(room_id) {
   room_id = convertIntoId(room_id);
-  var height = $('#' + room_id + '-msg')
-    .find('.Participants')
-    .css('height');
-  if (height == '0px') {
-    $('#' + room_id + '-msg')
-      .find('.Participants')
-      .css({ height: '60%', 'z-index': '+10' });
-    $('#' + room_id + '-msg')
-      .find('.btn')
-      .addClass('viewUsers');
+  var height = $("#" + room_id + "-msg")
+    .find(".Participants")
+    .css("height");
+  if (height == "0px") {
+    $("#" + room_id + "-msg")
+      .find(".Participants")
+      .css({ height: "60%", "z-index": "+10" });
+    $("#" + room_id + "-msg")
+      .find(".btn")
+      .addClass("viewUsers");
   } else {
-    $('#' + room_id + '-msg')
-      .find('.Participants')
-      .css({ height: '0%', 'z-index': '-10' });
-    $('#' + room_id + '-msg')
-      .find('.btn')
-      .removeClass('viewUsers');
+    $("#" + room_id + "-msg")
+      .find(".Participants")
+      .css({ height: "0%", "z-index": "-10" });
+    $("#" + room_id + "-msg")
+      .find(".btn")
+      .removeClass("viewUsers");
   }
 }
 
-var emo = document.querySelector('.smiley');
+var emo = document.querySelector(".smiley");
 
 function active() {
-  if ($('.smiley').hasClass('active')) {
-    $('.smiley').removeClass('active');
-    $('#emobox').css('display', 'none');
+  if ($(".smiley").hasClass("active")) {
+    $(".smiley").removeClass("active");
+    $("#emobox").css("display", "none");
   } else {
-    $('.smiley').addClass('active');
-    $('#emobox').css('display', 'block');
+    $(".smiley").addClass("active");
+    $("#emobox").css("display", "block");
   }
 }
 
-emo.addEventListener('click', active, false);
+emo.addEventListener("click", active, false);
 
 function writeEmoji(emoji) {
   var emojiName = emoji.id;
-  $('#textarea').val($('#textarea').val() + ':' + emojiName + ':');
-  $('#textarea').focus();
+  $("#textarea").val($("#textarea").val() + ":" + emojiName + ":");
+  $("#textarea").focus();
 }
 
-// For sidebar to work on small screens
-
-function closeSidebar() {
-  const navbar = document.querySelector('.left');
-  navbar.classList.add('showsidebar');
-  var toggle = document.querySelector('#toggle-icon');
-  toggle.className = '';
-  toggle.className += 'fa fa-angle-right';
-}
-
-function openSidebar() {
-  const navbar = document.querySelector('.left');
-  navbar.classList.remove('showsidebar');
-  var toggle = document.querySelector('#toggle-icon');
-  toggle.className = '';
-  toggle.className += 'fa fa-angle-left';
-}
+// For sidebar to work on small screens only
 
 function SidebarToggle() {
-  const navbar = document.querySelector('.left');
-  navbar.classList.toggle('showsidebar');
+  $(".left").addClass("showsidebar");
+  $(".overlay").addClass("overlay-show");
 }
 
+function overlayClick() {
+  $(".overlay").removeClass("overlay-show ");
+  $(".left").removeClass("showsidebar");
+}
